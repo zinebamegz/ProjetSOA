ce925e6bdd8a63b4c1827c336d3fc6516676bfe6
solene123456789 <solene123456789@gmail.com>
2022-01-13 11:23:07 +0100

    MAJ heater avec package .Model
    normalement, tout est ok pour heater normalement

--- HeaterActuator/HeaterActuator/src/main/java/fr/insa/HeaterActuator/Controller/HeaterActuatorRessource.java
+++ HeaterActuator/HeaterActuator/src/main/java/fr/insa/HeaterActuator/Controller/HeaterActuatorRessource.java
@@ -4,65 +4,43 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import fr.insa.HeaterActuator.Model.HeaterActuator;
 
 @RestController
 @RequestMapping("/HeaterActuator/")
 public class HeaterActuatorRessource {
-	//**************************** VARIABLES ****************************
+		
+	//creation of a ressource heater 
+	HeaterActuator heater = new HeaterActuator() ;
+		
 	
-	private boolean active; // true if heater on, false if heater off
-	private double temperatureToReach; //value up to which the heater should heat up
+	//**************************** METHODS ****************************
 	
-	//**************************** CONSTRUCTORS ****************************
-	public HeaterActuatorRessource() {
-		this.active = false; // not active by default
-		this.temperatureToReach = 25; // temperature to reach = 25°C by default
+	//return if the heater is active or not
+	@GetMapping("isActive/") 
+	public boolean isActive() {
+		return heater.getActive();
 	}
 	
-	public HeaterActuatorRessource(boolean activated, double temp) {
-		this.active = activated; 
-		this.temperatureToReach = temp; 
-	}
 	
-	public HeaterActuatorRessource(boolean activated) {
-		this.active = activated; 
-		this.temperatureToReach = 25.0; //default
+	//put on or off the heater according to a boolean parameter
+	@GetMapping("putHeaterOnOff/") 
+	public void putHeaterOnOff(boolean activate) {
+		heater.setActive(activate);
 	}
-	
-	public HeaterActuatorRessource(double temp) {
-		this.active = false; //default
-		this.temperatureToReach = temp; 
-	}
-	
-		//**************************** METHODS ****************************
-		
-		//return if the heater is active or not
-		
-		@GetMapping("getActive/") 
-		public boolean getActive() {
-			return this.active;
-		}
-		
-		
-		//put on or off the heater according to a boolean parameter
-		@GetMapping("setActive/") 
-		public void setActive(boolean activate) {
-			this.active = activate;
-		}
 
-		
-		//return the temperature to be reach
-		
-		@GetMapping("getTemperatureToReach/") 
-		public double getTemperatureToReach() { 
-			return temperatureToReach;
-		}
+	
+	//return the temperature to be reach
+	@GetMapping("getTemperatureToReach/") 
+	public double getTemperatureToReach() { 
+		return heater.getTemperatureToReach();
+	}
 
-		
-		//set the temperature to be reach
-		
-		@GetMapping(value="/setTemperatureToReach/{temp}")
-		public void setTemperatureToReach(@PathVariable double temp) {
-			this.temperatureToReach = temp;
-		}
+	
+	//set the temperature to be reach
+	
+	@GetMapping(value="/setTemperatureToReach/{temp}")
+	public void setTemperatureToReach(@PathVariable double temp) {
+		heater.setTemperatureToReach(temp);
+	}
 }
--- HeaterActuator/HeaterActuator/src/main/java/fr/insa/HeaterActuator/Model/HeaterActuator.java
+++ HeaterActuator/HeaterActuator/src/main/java/fr/insa/HeaterActuator/Model/HeaterActuator.java
@@ -1,5 +1,48 @@
 package fr.insa.HeaterActuator.Model;
 
 public class HeaterActuator {
+	//**************************** VARIABLES ****************************
+	
+		private boolean active; // true if heater on, false if heater off
+		private double temperatureToReach; //value up to which the heater should heat up
+		
+		//**************************** CONSTRUCTORS ****************************
+		public HeaterActuator() {
+			this.active = false; // not active by default
+			this.temperatureToReach = 25; // temperature to reach = 25°C by default
+		}
+		
+		public HeaterActuator(boolean activated, double temp) {
+			this.active = activated; 
+			this.temperatureToReach = temp; 
+		}
+		
+		public HeaterActuator(boolean activated) {
+			this.active = activated; 
+			this.temperatureToReach = 25.0; //default
+		}
+		
+		public HeaterActuator(double temp) {
+			this.active = false; //default
+			this.temperatureToReach = temp; 
+		}
 
+		//**************************** METHODS ****************************
+		
+		public boolean getActive() {
+			return active;
+		}
+
+		public void setActive(boolean active) {
+			this.active = active;
+		}
+
+		public double getTemperatureToReach() {
+			return temperatureToReach;
+		}
+
+		public void setTemperatureToReach(double temperatureToReach) {
+			this.temperatureToReach = temperatureToReach;
+		}
+		
 }
